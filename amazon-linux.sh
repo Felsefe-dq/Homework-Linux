2c2dc1e291c8cac0b45e9ec6252d9a3b7981f709
Felsefe-dq <devopsdatcom@gmail.com>
2024-09-01 22:40:06 +0000

    downloaded mariadb

--- amazon-linux.sh
+++ amazon-linux.sh
@@ -3,3 +3,6 @@ sudo yum install httpd -y
 sudo systemctl enable httpd
 cd /var/www/html
 sudo vi index.html
+sudo dnf install -y mariadb105-server
+wget https://wordpress.org/latest.tar.gz
+sudo systemctl start mariadb httpd
