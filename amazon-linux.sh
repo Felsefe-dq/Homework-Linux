7a942607a9337c862d923f195151f0737533ce47
EC2 Default User <ec2-user@ip-172-31-28-235.ec2.internal>
2024-09-07 06:28:20 +0000

    modified files

--- amazon-linux.sh
+++ amazon-linux.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 sudo yum install httpd -y
+sudo systemctl start httpd 
 sudo systemctl enable httpd
 systemctl status httpd
+sudo dnf install -y httpd wget php-fpm php-mysqli php-json php php-devel
+sudo dnf install -y mariadb105-server
+sudo systemctl is-enabled httpd
+wget https://wordpress.org/latest.tar.gz
+tar -xzf latest.tar.gz
+ls wordpress
+sudo cp -r wordpress/* /var/www/html
+ls /var/www/html
+sudo chown -R ec2-user:apache /var/www
+sudo systemctl start mariadb httpd
+sudo rm -r /var/www/html/index.html
 
