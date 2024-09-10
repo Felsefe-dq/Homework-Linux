e9cdd677e2c7f49da12d46644e75c07ae46975f5
EC2 Default User <ec2-user@ip-172-31-93-225.ec2.internal>
2024-09-10 22:08:29 +0000

    created a new file

---
+++ Homework12/calculator.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+num1=8
+num2=2
+
+echo "Enter the first number: $num1 "
+echo "Enter the second number: $num2"
+
+echo "The sum is $((num1+num2)) "
+echo "The difference is $((num1-num2)) "
+echo "The product is $((num1*num2)) "
+echo "The quotient is $((num1/num2)) "
