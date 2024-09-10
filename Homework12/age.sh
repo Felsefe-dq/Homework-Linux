fd1f739700ce430b770e1009578969c79514369d
EC2 Default User <ec2-user@ip-172-31-93-225.ec2.internal>
2024-09-10 22:24:40 +0000

    created a new script

---
+++ Homework12/age.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+year=2002
+
+echo "Enter your year of birth: $year "
+
+echo "How old the user will be in 2024? $((2024-$year)) years old "
+echo "How old the user was in 2020? $((2020-$year)) years old "
+echo "How old the user will be in 2030? $((2030-$year)) years old "
+
